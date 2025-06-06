b4bf6e41aa2684a19aa1ca8891303bbb8cee6290
nanjappa <csnanjappa@yahoo.com>
2025-06-06 18:15:31 +0300

    Add html output1

--- src/test/java/tests/CheckLisbonDevicesTest.java
+++ src/test/java/tests/CheckLisbonDevicesTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.experitest.appium.SeeTestClient;
+import com.google.common.collect.ImmutableMap;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -12,9 +13,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Stream;
 import org.json.JSONArray;
@@ -31,7 +30,7 @@ public class CheckLisbonDevicesTest {
     //    private static final String accessKey= "eyJhbGciOiJIUzI1NiJ9.eyJ4cC51Ijo0MiwieHAucCI6MywieHAubSI6MTcwMTAwMjYwNzIxMiwiZXhwIjoyMDE2MzYyNjA3LCJpc3MiOiJjb20uZXhwZXJpdGVzdCJ9.SMGYMR4IFha22QTlFFHg9UdyayG4kx4VcRHg7PjsYBY";
     private static final String cloudURL = "https://lisbon.experitest.com";
     private static final Queue<String> deviceInfoList = new ConcurrentLinkedQueue<>();
-    protected AppiumDriver driver = null;
+    private static ThreadLocal<AppiumDriver> driver = new ThreadLocal<>();
     protected SeeTestClient seetest = null;
 
     // Data Provider for parallel execution
@@ -55,6 +54,10 @@ public class CheckLisbonDevicesTest {
             if ("Available".equalsIgnoreCase(status) || "In Use".equalsIgnoreCase(status)) {
                 deviceData.add(new Object[]{ udid, os, deviceId, DHM, deviceName });
             }
+
+//            if ("Available".equalsIgnoreCase(status) && "iOS".equalsIgnoreCase(os)) {
+//                deviceData.add(new Object[]{ udid, os, deviceId, DHM, deviceName });
+//            }
         }
 
         return deviceData.toArray(new Object[0][0]);
@@ -70,75 +73,59 @@ public class CheckLisbonDevicesTest {
         dc.setCapability(MobileCapabilityType.UDID, udid);
         System.out.println("Thread " + Thread.currentThread().getId() + " -> Device: " + udid);
         // Release device
-        Unirest.post(cloudURL + "/" + deviceID + "/api/v1/devices/release")
-                .header("Authorization", "Bearer " + accessKey).asString();
+//        Unirest.post(cloudURL + "/" + deviceID + "/api/v1/devices/release").header("Authorization", "Bearer " + accessKey).asString();
         String deviceLanguage;
         String WiFI;
         if ("iOS".equalsIgnoreCase(deviceOS)) {
             dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
             dc.setCapability("bundleId", "com.apple.Preferences");
-            driver = new IOSDriver<>(new URL(cloudURL + "/wd/hub"), dc);
-            driver.launchApp();
-            Thread.sleep(2000);
-            String pageSource = driver.getPageSource().toLowerCase();
+            dc.setCapability("appiumVersion", "2.16.2");
+            driver.set(new IOSDriver<>(new URL(cloudURL + "/wd/hub"), dc));
+            driver.get().executeScript("mobile: terminateApp", ImmutableMap.of("bundleId", "com.apple.Preferences"));
+
+            Thread.sleep(1000);
+            driver.get().executeScript("mobile: activateApp", ImmutableMap.of("bundleId", "com.apple.Preferences"));
+            Thread.sleep(3000);
+            String pageSource = driver.get().getPageSource().toLowerCase();
             if (pageSource.toLowerCase().contains("id=\"settings\"")) {
                 deviceLanguage = "English";
             } else {
                 deviceLanguage = "NOT ENGLISH";
             }
-            String iOSWiFi =
-                    Stream.of("Faro", "Evora").filter(name -> pageSource.contains(name.toLowerCase())).findFirst()
-                            .orElse(null);
+            String iOSWiFi = Stream.of("Faro", "Evora").filter(name -> pageSource.contains(name.toLowerCase())).findFirst().orElse(null);
 
-            if (DHM.toLowerCase().contains("porto") && iOSWiFi != null && iOSWiFi.toLowerCase().contains("evora")) {
-                WiFI = "YES";
-            } else if (DHM.toLowerCase().contains("elvas") && iOSWiFi != null && iOSWiFi.toLowerCase()
-                    .contains("faro")) {
-                WiFI = "YES";
-            } else if (DHM.toLowerCase().contains("sintra") && iOSWiFi != null && iOSWiFi.toLowerCase()
-                    .contains("faro")) {
-                WiFI = "YES";
-            } else {
-                WiFI = "NO";
-            }
-            String log = "<tr>" +
-                    "<td>" + deviceName + "</td>" +
-                    "<td>" + WiFI + "</td>" +
-                    "<td>" + deviceLanguage + "</td>" +
-                    "</tr>";
+            if (DHM.toLowerCase().contains("porto") && iOSWiFi != null && iOSWiFi.toLowerCase().contains("evora")) {WiFI = "YES";}
+            else if (DHM.toLowerCase().contains("elvas") && iOSWiFi != null && iOSWiFi.toLowerCase().contains("faro")) {WiFI = "YES";}
+            else if (DHM.toLowerCase().contains("sintra") && iOSWiFi != null && iOSWiFi.toLowerCase().contains("faro")) {WiFI = "YES";}
+            else {WiFI = "WRONG WiFi";}
+            String log = "<tr>" + "<td>" + deviceName + "</td>" + "<td>" + WiFI + "</td>" + "<td>" + deviceLanguage + "</td>" + "</tr>";
             deviceInfoList.add(log);
-            driver.quit();
+            driver.get().quit();
 
         } else if ("Android".equalsIgnoreCase(deviceOS)) {
             dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
             dc.setCapability("appPackage", "com.android.settings");
             dc.setCapability("appActivity", "com.android.settings.Settings");
-            driver = new AndroidDriver<>(new URL(cloudURL + "/wd/hub"), dc);
-            seetest = new SeeTestClient(driver);
-            String result = seetest.run("adb -s " + udid + " shell dumpsys wifi | grep \"SSID\"");
+            dc.setCapability("appiumVersion", "2.16.2");
+            driver.set(new AndroidDriver<>(new URL(cloudURL + "/wd/hub"), dc));
+
+            String result = (String) driver.get().executeScript("mobile: shell", Map.of("command", "dumpsys", "args", List.of("wifi")));
+            String ssid = Arrays.stream(result.split("\n")).map(String::trim).filter(line -> line.contains("SSID")).findFirst().orElse("SSID not found");
+
             Thread.sleep(1500);
-            deviceLanguage = seetest.run("adb -s " + udid + " shell getprop | grep -i persist.sys.locale");
 
-            String[] parts = result.split(",");
-            String androidWiFI = parts[0].trim();
+            result = (String) driver.get().executeScript("mobile: shell", Map.of("command", "getprop", "args", List.of()));
+            deviceLanguage = Arrays.stream(result.split("\n")).map(String::trim).filter(line -> line.toLowerCase().contains("persist.sys.locale")).findFirst().orElse("Not found");
 
-            if (DHM.toLowerCase().contains("porto") && androidWiFI.toLowerCase().contains("evora")) {
-                WiFI = "YES";
-            } else if (DHM.toLowerCase().contains("elvas") && androidWiFI.toLowerCase().contains("faro")) {
-                WiFI = "YES";
-            } else if (DHM.toLowerCase().contains("sintra") && androidWiFI.toLowerCase().contains("faro")) {
-                WiFI = "YES";
-            } else {
-                WiFI = "NO";
-            }
 
-            String log = "<tr>" +
-                    "<td>" + deviceName + "</td>" +
-                    "<td>" + WiFI + "</td>" +
-                    "<td>" + deviceLanguage + "</td>" +
-                    "</tr>";
+            if (DHM.toLowerCase().contains("porto") && ssid.toLowerCase().contains("evora")) {WiFI = "YES";}
+            else if (DHM.toLowerCase().contains("elvas") && ssid.toLowerCase().contains("faro")) {WiFI = "YES";}
+            else if (DHM.toLowerCase().contains("sintra") && ssid.toLowerCase().contains("faro")) {WiFI = "YES";}
+            else {WiFI = "NO";}
+
+            String log = "<tr>" + "<td>" + deviceName + "</td>" + "<td>" + WiFI + "</td>" + "<td>" + deviceLanguage + "</td>" + "</tr>";
             deviceInfoList.add(log);
-            driver.quit();
+            driver.get().quit();
         }
     }
 
@@ -148,6 +135,7 @@ public class CheckLisbonDevicesTest {
         System.out.println("<html><body><table border=1>");
         String dateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd MMM yyyy HH:mm:ss"));
         System.out.println("<tr><td> Lisbon cloud monitoring report </td><td>" + dateTime + "</td></tr>");
+        System.out.println("<tr>" + "<td> Device Name </td>" + "<td> Correct WiFi? </td>" + "<td> Device Language </td>" + "</tr>");
         deviceInfoList.forEach(System.out::println);
         System.out.println("</table></body></html>");
         System.out.println("end-here");
