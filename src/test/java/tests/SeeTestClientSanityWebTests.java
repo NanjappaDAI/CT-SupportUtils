2ad94b5e54ffca227899dd575cf0f9c60c69aba5
nanjappa <csnanjappa@yahoo.com>
2026-05-21 19:34:04 +0300

    Report failed tests

--- src/test/java/tests/SeeTestClientSanityWebTests.java
+++ src/test/java/tests/SeeTestClientSanityWebTests.java
@@ -2,15 +2,18 @@ package tests;
 
 import com.experitest.client.Client;
 import com.experitest.client.GridClient;
+import com.experitest.client.Utils;
+import com.experitest.client.log.Level;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.annotations.AfterMethod;
@@ -21,13 +24,17 @@ import org.testng.annotations.Test;
 public class SeeTestClientSanityWebTests {
 
     private static final String cloudURL = "https://uscloud.experitest.com";
-    private static final Queue<String> iOSDeviceInfoList = new ConcurrentLinkedQueue<>();
+    String accessKey = "aut_1_BOam4oOXuJFMnR8c3JOBJ6gDILqfBf80vvsZPO8-CHQ=";
+
+//    private static final String cloudURL = "https://lisbon.experitest.com";
+//    String accessKey = "aut_1_0mSJdlr88QCFpa-2LJ28I3wXQhpi0Brmqof-V2g7Kyw=";
+
+    private static final Queue<String> failedTestsList = new ConcurrentLinkedQueue<>();
     private static final Queue<String> androidDeviceInfoList = new ConcurrentLinkedQueue<>();
     private final ThreadLocal<Client> client = new ThreadLocal<>();
     private final ThreadLocal<GridClient> grid = new ThreadLocal<>();
 
-    String accessKey = "aut_1_BOam4oOXuJFMnR8c3JOBJ6gDILqfBf80vvsZPO8-CHQ=";
-
+    private boolean GoogleValidated = false;
 
     public SeeTestClientSanityWebTests() {
     }
@@ -52,14 +59,13 @@ public class SeeTestClientSanityWebTests {
                 deviceData.add(new Object[]{ udid, os, deviceId, testNameSuffix });
             }
         }
-        for (Object[] device : deviceData) {
-            System.out.println(Arrays.toString(device));
-        }
+        System.out.println("Total Available devices : " + deviceData.size());
         return deviceData.toArray(new Object[0][0]);
     }
 
     @Test(dataProvider = "devices")
     public void getDeviceHealth(String udid, String deviceOS, String deviceID, String testNameSuffix) {
+        StringBuilder log = new StringBuilder();
         GridClient grid = new GridClient(accessKey, cloudURL);
         String testName = "Seetest Client Web Test_" + testNameSuffix;
         String deviceQuery = "@serialnumber='" + udid + "'";
@@ -67,21 +73,13 @@ public class SeeTestClientSanityWebTests {
         client.set(seetestClient);
         client.get().setReporter("xml", "", testName);
 
-        String log;
         if ("iOS".equalsIgnoreCase(deviceOS)) {
-            client.get().launch("safari:http://google.com", true, false);
+            iOSWebTest();
         } else {
-            client.get().launch("chrome:http://google.com", true, false);
+            androidWebTest();
         }
-        client.get().sleep(10000);
-        if (client.get().isElementFound("WEB", "//*[@name='q' or @aria-label='Search']", 0)) {
-            client.get().report("Google home page seen", true);
-            boolean googleValidated = true;
-        } else {
-            client.get().report("Google home page NOT seen", false);
-        }
-        String apiUrl = cloudURL + "/api/v1/devices/" + deviceID + "/http-request";
 
+        String apiUrl = cloudURL + "/api/v1/devices/" + deviceID + "/http-request";
         HttpResponse<String> response;
         try {
             response = Unirest.post(apiUrl).header("Authorization", "Bearer " + accessKey)
@@ -90,7 +88,78 @@ public class SeeTestClientSanityWebTests {
         } catch (UnirestException e) {
             throw new RuntimeException(e);
         }
-        client.get().report("API validation for returned Response: " + response.getStatusText(), response.getStatus() == 200);
+
+        if (response.getStatus() == 200) {
+            client.get().report("API validation returned 200 OK", true);
+        } else {
+            client.get().report("API validation returned unexpected error | HTTP " + response.getStatus() , false);
+        }
+
+        String status = GoogleValidated ? "PASSED" : "FAILED";
+        String msg = "Validation " + status + " | Google: " + (GoogleValidated ? "OK" : "FAIL");
+        client.get().setReportStatus(status, msg);
+
+        if (status.equals("FAILED")) {
+            client.get().setLogger(Utils.initDefaultLogger(Level.OFF));
+            String xml = client.get().getVisualDump("Web");
+            Pattern pattern = Pattern.compile("text=\"([^\"]*)\"");
+            Matcher matcher = pattern.matcher(xml);
+            int count = 0;
+            while (matcher.find() && count < 5) {
+                String text = matcher.group(1).trim();
+                if (!text.isBlank()) {
+                    if (log.length() > 0) {
+                        log.append(" ");
+                    }
+                    log.append(text);
+                    count++;
+                }
+            }
+        }
+        failedTestsList.add(String.valueOf(log));
+    }
+
+    private void androidWebTest() {
+        client.get().launch("chrome:http://google.com/ncr", true, false);
+        client.get().sleep(10000);
+        client.get().waitForElement("NATIVE", "//*[contains(@name,'Can') and contains(@name,'Open Page')]", 0, 15000);
+        if (client.get().isElementFound("WEB", "//*[@text='Sign in']", 0)) {
+            client.get().report("Google home page seen", true);
+            GoogleValidated = true;
+        } else if (client.get()
+                .isElementFound("NATIVE", "//*[contains(@name,'Can') and contains(@name,'Open Page')]", 0)) {
+            client.get().report("Can't open page element detected", false);
+        } else if (client.get().isElementFound("WEB", "//*[@text='Got it']", 0)) {
+            client.get().report("Google home page seen", true);
+            GoogleValidated = true;
+        } else if (client.get().isElementFound("WEB", "//*[@aria-label='Google' or @text='Sign in']", 0)) {
+            client.get().report("Google home page seen", true);
+            GoogleValidated = true;
+        } else {
+            client.get().report("Google home page NOT seen", false);
+        }
+    }
+
+    private void iOSWebTest() {
+        client.get().launch("safari:http://google.com", true, false);
+        client.get().sleep(10000);
+        client.get().waitForElement("NATIVE",
+                "//*[@class='UIAView' and contains(@name,'Can') and contains(@name,'Open Page')]", 0, 15000);
+        if (client.get().isElementFound("NATIVE",
+                "//*[@class='UIAView' and contains(@name,'Can') and contains(@name,'Open Page')]", 0)) {
+            client.get().report("Can't open page element detected", false);
+        } else if (client.get().isElementFound("WEB", "//*[@text='Sign in']", 0)) {
+            client.get().report("Google home page seen", true);
+            GoogleValidated = true;
+        } else if (client.get().isElementFound("WEB", "//*[@text='Got it']", 0)) {
+            client.get().report("Google home page seen", true);
+            GoogleValidated = true;
+        } else if (client.get().isElementFound("WEB", "//*[@aria-label='Google' or @text='Sign in']", 0)) {
+            client.get().report("Google home page seen", true);
+            GoogleValidated = true;
+        } else {
+            client.get().report("Google home page NOT seen", false);
+        }
     }
 
     @AfterMethod(alwaysRun = true)
@@ -114,6 +183,7 @@ public class SeeTestClientSanityWebTests {
     @AfterSuite
     public void printAllCollectedDeviceIDs() {
         System.out.println("start-here");
+        failedTestsList.forEach(System.out::println);
         System.out.println("end-here");
     }
 }
